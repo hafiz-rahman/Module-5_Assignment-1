6782f78235b4762701f522ff55d94d34904c12b7
Md. Hafizur Rahman <hafiz-rahman@live.com>
2024-07-08 21:13:32 +0600

    Correct Styles file

--- celsius_to_fahrenheit.php
+++ celsius_to_fahrenheit.php
@@ -1,9 +1,9 @@
 <?php
-// Define the temperature in Celsius
+// Defined temperature
 $celsius = 32;
 
 // Using Celsius to Fahrenheit conversion formula: F = (C * 9/5) + 32
 $fahrenheit = ($celsius * 9 / 5) + 32;
 
-// Output the temperature in Fahrenheit with 2 decimal points
+// Fahrenheit with 2 decimal points
 printf("The temperature %.2f degrees Celsius is %.2f degrees Fahrenheit.", $celsius, $fahrenheit);
--- largest_number.php
+++ largest_number.php
@@ -1,21 +1,19 @@
 <?php
-// Define the three numbers
+// Three defined numbers
 $num1 = 4;
 $num2 = 5;
 $num3 = 6;
 
-// Check if $num1 is greater than or equal to both $num2 and $num3
+// Checking if $num1 is greater than or equal to both $num2 and $num3
 if ($num1 >= $num2 && $num1 >= $num3) {
-  // If true, $num1 is the largest
   $largest = $num1;
 } elseif ($num2 >= $num1 && $num2 >= $num3) {
-  // Otherwise, check if $num2 is greater than or equal to both $num1 and $num3
-  // If true, $num2 is the largest
+
+  // Checking if $num2 is greater than or equal to both $num1 and $num3
   $largest = $num2;
 } else {
-  // If neither of the above conditions are true, $num3 must be the largest
   $largest = $num3;
 }
 
-// Output the largest number
+// Output
 echo "The largest number among {$num1}, {$num2}, and {$num3} is {$largest}.";
